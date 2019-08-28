637b4860fb3d8132257776af57b55eb309919036
= <leobeckerdaluz@gmail.com>
2019-08-27 23:58:56 -0300

    blink_task+motor_tests_comentados

--- src/main.c
+++ src/main.c
@@ -56,9 +56,111 @@ static void echo_task()
         // Write data back to the UART
         uart_write_bytes(UART_NUM_1, (const char *) data, len);
     }
+
+
+    // #define LEFT_FRONT 27
+    // #define RIGHT_FRONT 26
+    // #define LEFT_BACK 25
+    // #define RIGHT_BACK 33
+    // #define DELAY_OFF 1000
+    // #define DELAY_ON 500
+
+    // gpio_pad_select_gpio(LEFT_FRONT);
+    // gpio_set_direction(LEFT_FRONT, GPIO_MODE_OUTPUT);
+    // gpio_pad_select_gpio(RIGHT_FRONT);
+    // gpio_set_direction(RIGHT_FRONT, GPIO_MODE_OUTPUT);
+    // gpio_pad_select_gpio(LEFT_BACK);
+    // gpio_set_direction(LEFT_BACK, GPIO_MODE_OUTPUT);
+    // gpio_pad_select_gpio(RIGHT_BACK);
+    // gpio_set_direction(RIGHT_BACK, GPIO_MODE_OUTPUT);
+
+    // while (1) {
+    //     // Read data from the UART
+    //     int len = uart_read_bytes(UART_NUM_1, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+    //     // Write data back to the UART
+    //     uart_write_bytes(UART_NUM_1, (const char *) data, len);
+
+    //     if (len > 0){
+    //         int i = 0;
+
+    //         printf("Chegou!\n");
+
+    //         char* bt_data = 0;
+    //         bt_data = (char*)malloc(len);
+    //         memcpy(bt_data,data,len);
+    //         bt_data[len-1] = '\0'; 
+    //         printf("%s\n", bt_data);
+
+    //         char bt_data_char = (char) bt_data[0];
+
+    //         if (bt_data_char == '3'){
+    //             printf("LEFT!\n");
+    //             gpio_set_level(LEFT_BACK, 1);
+    //             gpio_set_level(RIGHT_FRONT, 1);
+    //             vTaskDelay(DELAY_ON / portTICK_PERIOD_MS);
+    //             gpio_set_level(LEFT_BACK, 0);
+    //             gpio_set_level(RIGHT_FRONT, 0);
+    //             vTaskDelay(DELAY_OFF / portTICK_PERIOD_MS);
+    //         }
+    //         else if (bt_data_char == '4'){
+    //             printf("RIGHT!\n");
+    //             gpio_set_level(RIGHT_BACK, 1);
+    //             gpio_set_level(LEFT_FRONT, 1);
+    //             vTaskDelay(DELAY_ON / portTICK_PERIOD_MS);
+    //             gpio_set_level(RIGHT_BACK, 0);
+    //             gpio_set_level(LEFT_FRONT, 0);
+    //             vTaskDelay(DELAY_OFF / portTICK_PERIOD_MS);
+    //         }
+    //         else if (bt_data_char == '1'){
+    //             printf("STRAIGHT!\n");
+    //             gpio_set_level(LEFT_FRONT, 1);
+    //             gpio_set_level(RIGHT_FRONT, 1);
+    //             vTaskDelay(DELAY_ON / portTICK_PERIOD_MS);
+    //             gpio_set_level(LEFT_FRONT, 0);
+    //             gpio_set_level(RIGHT_FRONT, 0);
+    //             vTaskDelay(DELAY_OFF / portTICK_PERIOD_MS);
+    //         }
+    //         else if (bt_data_char == '2'){
+    //             printf("BACK!\n");
+    //             gpio_set_level(LEFT_BACK, 1);
+    //             gpio_set_level(RIGHT_BACK, 1);
+    //             vTaskDelay(DELAY_ON / portTICK_PERIOD_MS);
+    //             gpio_set_level(LEFT_BACK, 0);
+    //             gpio_set_level(RIGHT_BACK, 0);
+    //             vTaskDelay(DELAY_OFF / portTICK_PERIOD_MS);
+    //         }
+            
+    //         printf("-------------------\n");
+    //     }
+    // }
+}
+
+void blink_task(void *pvParameter)
+{
+    #define BLINK_GPIO 2
+    #define BLINK_DELAY 300
+
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+    }
 }
 
 void app_main()
 {
+    // xTaskCreate(&blink_task, "blink_task", 1024, NULL, 5, NULL);
     xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
 }
