eca296d135f1c95d1420e87e9e1eec53e882b00e
= <leobeckerdaluz@gmail.com>
2019-09-17 22:51:03 -0300

    encoder_TIME+RPM

--- ADAS_Rasp/encoder.py
+++ ADAS_Rasp/encoder.py
@@ -6,20 +6,41 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 #defines
-VERIFY_INTERVAL = 1 #s
+VERIFY_INTERVAL = 10 #s
 GPIO_LEFT_ENCODER = 4
+
+COUNT_LIMIT = 20
 
 #set GPIO direction (IN / OUT)
 GPIO.setup(GPIO_LEFT_ENCODER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 setado = False
 global_count = 0
+StartTime = time.time()
+StopTime = time.time()
 
 def my_callback(channel):
     global global_count
+    global StartTime
+    global StopTime
+
     global_count += 1
 
     print (".")
+
+    if(global_count >= COUNT_LIMIT):
+        # Stop time
+        StopTime = time.time()
+        # Zera contador
+        global_count = 0
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        print ("TIME: " + str(TimeElapsed))
+        # Calcula o RPM
+        RPM = 1/TimeElapsed*60
+        print ("RPM: " + str(RPM))
+        # Inicia a contagem novamente
+        StartTime = time.time()
 
 # when a falling edge is detected on port 17, regardless of whatever   
 # else is happening in the program, the function my_callback will be run  
@@ -27,8 +48,10 @@ GPIO.add_event_detect(GPIO_LEFT_ENCODER, GPIO.FALLING, callback=my_callback, bou
 
 if __name__ == '__main__':
     try:
+        
+
         while True:
-            print ("Verificando encoder!")
+            print ("ON!")
             time.sleep(VERIFY_INTERVAL)
  
         # Reset by pressing CTRL + C
