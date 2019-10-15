4eb2bfbed08486cb3ca9c75f3942b76229035fff
leobeckerdaluz <leobeckerdaluz@gmail.com>
2019-10-15 09:10:16 -0300

    GPIO_test

--- ADAS_Rasp/main.py
+++ ADAS_Rasp/main.py
@@ -6,13 +6,15 @@ import random
 from multiprocessing import Lock
 # from multiprocessing import Process, Lock
 import threading 
-# import statistics
+import statistics
 import numpy as np
 
 # Constants
 AUTOBRAKE_DIST = 7.0 #cm
 COLLISION_DIST = 15.0 #cm
 VERIFY_INTERVAL = 0.1 #s
+SENSOR_READ_INTERVAL = 0.00001 #s
+GET_MEDIAN_INTERVAL = 0.5 #s
 
 #set GPIO Pins
 GPIO_TRIGGER_0 = 25
@@ -37,6 +39,9 @@ camera_distances = []
 
 def sensor_thread(ID, GPIO_TRIGGER, GPIO_ECHO):
     print(str(ID) + ": Chegou " + str(GPIO_TRIGGER) + " e " + str(GPIO_ECHO))
+
+    if ((ID == 1) or (ID == 2)):
+        return
 
     if __debug__:
         while True:
@@ -64,21 +69,24 @@ def sensor_thread(ID, GPIO_TRIGGER, GPIO_ECHO):
                 lock_2.release()
                 time.sleep(0.00001)
     else:
-        with mutex_GPIO_WRITE:
-            #set GPIO direction
-            GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-            GPIO.setup(GPIO_ECHO, GPIO.IN)
+        mutex_GPIO_WRITE.acquire()
+        #set GPIO direction
+        GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO, GPIO.IN)
+        mutex_GPIO_WRITE.release()
 
         while True:
-            with mutex_GPIO_WRITE:
-                # set Trigger to HIGH
-                GPIO.output(GPIO_TRIGGER, True)
+            mutex_GPIO_WRITE.acquire()
+            # set Trigger to HIGH
+            GPIO.output(GPIO_TRIGGER, True)
+            mutex_GPIO_WRITE.release()
         
             # set Trigger after 0.01ms to LOW
-            time.sleep(0.00001)
+            time.sleep(SENSOR_READ_INTERVAL)
             
-            with mutex_GPIO_WRITE:
-                GPIO.output(GPIO_TRIGGER, False)
+            mutex_GPIO_WRITE.acquire()
+            GPIO.output(GPIO_TRIGGER, False)
+            mutex_GPIO_WRITE.release()
         
             StartTime = time.time()
             StopTime = time.time()
@@ -127,6 +135,12 @@ def image_processing_thread(ID):
     else:    
         try:
             blablabla_frame = 20
+            dist = random.uniform(20.0, 25.0)
+            lock_camera.acquire()
+            camera_distances.append(dist)
+            lock_camera.release()
+            time.sleep(0.1)
+
         # Reset by pressing CTRL + C
         except KeyboardInterrupt:
             print("Measurement stopped by User")
@@ -136,80 +150,84 @@ def image_processing_thread(ID):
 def get_median_distances_thread(ID): 
     print("{}: Real Distances".format(ID))
 
-    if __debug__:
-        while True:
-            lock_camera.acquire()
-            camera_len = len(camera_distances)
-            # camera_median = statistics.median(camera_distances)
-            camera_median = np.median(camera_distances)
-            # op = np.median(dataset)
-            camera_distances.clear()
-            lock_camera.release()
-            
-            lock_0.acquire()
-            sensor0_len = len(sensor0_distances)
-            # sensor0_median = statistics.median(sensor0_distances)
-            sensor0_median = np.median(sensor0_distances)
-            sensor0_distances.clear()
-            lock_0.release()
-            
-            lock_1.acquire()
-            sensor1_len = len(sensor1_distances)
-            # sensor1_median = statistics.median(sensor1_distances)
-            sensor1_median = np.median(sensor1_distances)
-            sensor1_distances.clear()
-            lock_1.release()
-            
-            lock_2.acquire()
-            sensor2_len = len(sensor2_distances)
-            # sensor2_median = statistics.median(sensor2_distances)
-            sensor2_median = np.median(sensor2_distances)
-            sensor2_distances.clear()
-            lock_2.release()
-            print("-----------------------")
+    while True:
+        lock_camera.acquire()
+        camera_len = len(camera_distances)
+        camera_median = 0
+        if (len(camera_distances) > 0):
+            camera_median = statistics.median(camera_distances)
+        # camera_median = np.median(camera_distances)
+        camera_distances.clear()
+        lock_camera.release()
+        
+        lock_0.acquire()
+        sensor0_len = len(sensor0_distances)
+        sensor0_median = 0
+        if (len(sensor0_distances) > 0):
+            sensor0_median = statistics.median(sensor0_distances)
+        # sensor0_median = np.median(sensor0_distances)
+        sensor0_distances.clear()
+        lock_0.release()
+        
+        lock_1.acquire()
+        sensor1_len = len(sensor1_distances)
+        sensor1_median = 0
+        if (len(sensor1_distances) > 0):
+            sensor1_median = statistics.median(sensor1_distances)
+        # sensor1_median = np.median(sensor1_distances)
+        sensor1_distances.clear()
+        lock_1.release()
+        
+        lock_2.acquire()
+        sensor2_len = len(sensor2_distances)
+        sensor2_median = 0
+        if (len(sensor2_distances) > 0):
+            sensor2_median = statistics.median(sensor2_distances)
+        # sensor2_median = np.median(sensor2_distances)
+        sensor2_distances.clear()
+        lock_2.release()
+        print("-----------------------")
 
-            if ((sensor0_median < AUTOBRAKE_DIST) or 
-                (sensor1_median < AUTOBRAKE_DIST) or 
-                (sensor2_median < AUTOBRAKE_DIST) or
-                (camera_median < AUTOBRAKE_DIST)):
-                
-                print("AUTOBRAKE!")
-                if not __debug__:
-                    with mutex_GPIO_WRITE:
-                        GPIO.output(GPIO_AUTOBRAKE, True)
-                        GPIO.output(GPIO_COLLISION, True)
-
-            elif ((sensor0_median < COLLISION_DIST) or 
-                (sensor1_median < COLLISION_DIST) or 
-                (sensor2_median < COLLISION_DIST) or
-                (camera_median < COLLISION_DIST)):
-                
-                print("COLLISION!")
-                if not __debug__:
-                    with mutex_GPIO_WRITE:
-                        GPIO.output(GPIO_AUTOBRAKE, False)
-                        GPIO.output(GPIO_COLLISION, True)
-            else:
-                print("NORMAL!")
-                if not __debug__:
-                    with mutex_GPIO_WRITE:
-                        GPIO.output(GPIO_AUTOBRAKE, False)
-                        GPIO.output(GPIO_COLLISION, False)
+        if ((sensor0_median < AUTOBRAKE_DIST) or 
+            (sensor1_median < AUTOBRAKE_DIST) or 
+            (sensor2_median < AUTOBRAKE_DIST) or
+            (camera_median < AUTOBRAKE_DIST)):
             
-            print("cont camera: {}".format(camera_len))
-            print("dist camera: {}".format(camera_median))
-            print("cont sensor0: {}".format(sensor0_len))
-            print("dist sensor0: {}".format(sensor0_median))
-            print("cont sensor1: {}".format(sensor1_len))
-            print("dist sensor1: {}".format(sensor1_median))
-            print("cont sensor2: {}".format(sensor2_len))
-            print("dist sensor2: {}".format(sensor2_median))
-            time.sleep(0.5)
-    else:    
-        blablabla_frame = 20
-        if not __debug__:
-            GPIO.cleanup()
-            # As far as I am aware only the RPi.GPIO and RPIO.GPIO Python modules have a cleanup method. The cleanup method sets all the gpios you have used to be inputs and disables the internal pull-ups/downs for those gpios.
+            print("AUTOBRAKE!")
+            if not __debug__:
+                mutex_GPIO_WRITE.acquire()
+                GPIO.output(GPIO_AUTOBRAKE, True)
+                GPIO.output(GPIO_COLLISION, True)
+                mutex_GPIO_WRITE.release()
+
+        elif ((sensor0_median < COLLISION_DIST) or 
+            (sensor1_median < COLLISION_DIST) or 
+            (sensor2_median < COLLISION_DIST) or
+            (camera_median < COLLISION_DIST)):
+            
+            print("COLLISION!")
+            if not __debug__:
+                mutex_GPIO_WRITE.acquire()
+                GPIO.output(GPIO_AUTOBRAKE, False)
+                GPIO.output(GPIO_COLLISION, True)
+                mutex_GPIO_WRITE.release()
+        else:
+            print("NORMAL!")
+            if not __debug__:
+                mutex_GPIO_WRITE.acquire()
+                GPIO.output(GPIO_AUTOBRAKE, False)
+                GPIO.output(GPIO_COLLISION, False)
+                mutex_GPIO_WRITE.release()
+        
+        print("cont camera: {}".format(camera_len))
+        print("dist camera: {}".format(camera_median))
+        print("cont sensor0: {}".format(sensor0_len))
+        print("dist sensor0: {}".format(sensor0_median))
+        #print("cont sensor1: {}".format(sensor1_len))
+        #print("dist sensor1: {}".format(sensor1_median))
+        #print("cont sensor2: {}".format(sensor2_len))
+        #print("dist sensor2: {}".format(sensor2_median))
+        time.sleep(GET_MEDIAN_INTERVAL)
 
 if __name__ == '__main__':
     if not __debug__:
@@ -222,19 +240,24 @@ if __name__ == '__main__':
     sensor0_thread = threading.Thread(target=sensor_thread, args=(0, GPIO_TRIGGER_0, GPIO_ECHO_0,))
     # sensor0_thread = Process(target=sensor_thread, args=(0, GPIO_TRIGGER_0, GPIO_ECHO_0,))
     sensor0_thread.start()
+    # sensor0_thread.join()
     
     sensor1_thread = threading.Thread(target=sensor_thread, args=(1, GPIO_TRIGGER_1, GPIO_ECHO_1,))
     # sensor1_thread = Process(target=sensor_thread, args=(1, GPIO_TRIGGER_1, GPIO_ECHO_1,))
     sensor1_thread.start()
+    # sensor1_thread.join()
     
     sensor2_thread = threading.Thread(target=sensor_thread, args=(2, GPIO_TRIGGER_2, GPIO_ECHO_2,))
     # sensor2_thread = Process(target=sensor_thread, args=(2, GPIO_TRIGGER_2, GPIO_ECHO_2,))
     sensor2_thread.start()
+    # sensor2_thread.join()
     
     image_processing_thread = threading.Thread(target=image_processing_thread, args=(3,))
     # image_processing_thread = Process(target=image_processing_thread, args=(3,))
     image_processing_thread.start()
+    # image_processing_thread.join()
     
     get_median_distances_thread = threading.Thread(target=get_median_distances_thread, args=(4,))
     # get_mean_distances_thread = Process(target=get_mean_distances_thread, args=(4,))
     get_median_distances_thread.start()
+    # get_median_distances_thread.join()
